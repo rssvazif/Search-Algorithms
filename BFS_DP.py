e678f31956b20cf0a47f528d2172adad8eccedf9
Reza <rezavazifedost@gmail.com>
2024-12-14 16:27:09 +0330

    it's one of the have dynamic inputs , but it may be more optimal.

---
+++ BFS_DP.py
@@ -0,0 +1,29 @@
+
+graph = {}
+
+visited = []
+queue = []
+start = 'A'
+
+def bfs_algorithm(visited,queue,start):
+    target = input('please enter your target!!')
+    queue.append(start)
+    while queue:
+        p = queue.pop(0)
+        if p == target:
+            visited.append(p)
+            return visited
+        else:
+            visited.append(p)
+            for element in graph[p]:
+                queue.append(element)
+
+count_node = int(input('please enter nodes of your graph!'))
+for j in range(count_node):
+    parent = input('enter the parent!')
+    children = list(input('enter children!'))
+    graph[parent] = children
+
+solution = bfs_algorithm(visited,queue,start)
+for i in solution:
+    print(i,end=" ")
